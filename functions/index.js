66e147ae6c34493f5524975dd91aca9859710399
adesanya221 <adesanyafisayo112@gmail.com>
2025-04-02 14:52:46 +0100

    Initial commit: Foodie Mobile App

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-const functions = require("firebase-functions");
-
-// Function to generate a challenge
-exports.generateChallenge = functions.https.onRequest((req, res) => {
-  const challenge = crypto.randomBytes(32).toString("base64");
-  res.json({challenge});
-});
